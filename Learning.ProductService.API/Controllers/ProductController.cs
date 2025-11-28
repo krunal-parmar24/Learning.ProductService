9d44895eb10f4a89934bd9094a8e06c8fcefc5c1
krunal-parmar24 <krunalrparmar4@gmail.com>
2025-11-28 10:28:59 +0530

    Added basic product controller and removed other unwanted files

---
+++ Learning.ProductService.API/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learning.ProductService.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<string> { "Iphone 17", "Samsung Galaxy"});
+        }
+    }
+}
