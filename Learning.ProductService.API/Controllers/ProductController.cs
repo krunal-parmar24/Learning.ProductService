7ea5bb8f5b8123f42f4e830d2462bd731b1b2161
krunal-parmar24 <krunalrparmar4@gmail.com>
2025-12-15 23:09:51 +0530

    Updated Route from "products" to "api/v1/products" as due to minor changes directly pushed into master

--- Learning.ProductService.API/Controllers/ProductController.cs
+++ Learning.ProductService.API/Controllers/ProductController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Learning.ProductService.API.Controllers
 {
     [ApiController]
-    [Route("products")]
+    [Route("api/v1/products")]
     public class ProductController : Controller
     {
         private readonly IMediator _mediator;
